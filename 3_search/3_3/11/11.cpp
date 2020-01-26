c3ab71d9ff2a36ff024f1c8e1cf8ce73132d3c80
evasilyev <vasilev.evgeniy@physics.msu.ru>
2020-01-27 00:20:28 +0300

    3_search/3_3/11

---
+++ 3_search/3_3/11/11.cpp
@@ -0,0 +1,56 @@
+//
+// Created by evasilyev on 26.01.2020.
+//
+
+#include <iostream>
+#include <vector>
+
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    os << "]\n";
+    return os;
+}
+
+int FindTargetNumber(std::vector<int>& v, int x) {
+    int l = 0;
+    int r = (int)v.size() - 1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (x < v[m]) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return (int)v.size() - l;
+
+}
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    std::vector<int> v(N);
+    for (size_t i = 0; i < N; ++i) {
+        std::cin >> v[i];
+    }
+
+    int M;
+    std::cin >> M;
+
+    int x;
+    for (size_t i = 0; i < M; ++i) {
+        std::cin >> x;
+        std::cout << FindTargetNumber(v, x) << std::endl;
+    }
+
+    return 0;
+}
