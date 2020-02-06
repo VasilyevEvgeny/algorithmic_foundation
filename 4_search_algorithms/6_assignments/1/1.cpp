fbf75db6e9830f08469ba4b059a60333831e81bf
evasilyev <vasilev.evgeniy@physics.msu.ru>
2020-02-06 09:40:21 +0300

    4_search_algorithms__5_binary_search_by_answer__6__1

---
+++ 4_search_algorithms/6_assignments/1/1.cpp
@@ -0,0 +1,49 @@
+//
+// Created by evasilyev on 05.02.2020.
+//
+
+#include <iostream>
+
+
+int formula(int n, int a, int b, int x) {
+    double y = ((double)(n - a * x)) / b;
+    int y_integer = (int)y;
+    return ((y - y_integer) == 0.0) ? y_integer : -1;
+}
+
+int binary(int n, int a, int b, int x) {
+    int l = 0, r = n;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        int tmp = a * x + b * m;
+        if (tmp < n) {
+            l = m + 1;
+        }
+        else if (tmp > n) {
+            r = m - 1;
+        }
+        else {
+            return m;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int n = 0, a = 0, b = 0;
+    std::cin >> n >> a >> b;
+
+    int s = 0;
+    for (int x = 0; x <= n; ++x) {
+//        int y = formula(n, a, b, x);
+        int y = binary(n, a, b, x);
+        if (y >= 0) {
+            s++;
+        }
+    }
+    std::cout << s;
+
+    return 0;
+}
