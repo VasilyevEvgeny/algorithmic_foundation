ba41676555cf1c1e9acf9a5f65f1b9778f41e9b0
evasilyev <vasilev.evgeniy@physics.msu.ru>
2020-03-08 00:34:21 +0300

    6_two_pointer_method__6_assignments__3

--- 6_two_pointer_method/6_assignments/3/3.cpp
+++ 6_two_pointer_method/6_assignments/3/3.cpp
@@ -6,7 +6,15 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <algorithm>
+#include <random>
+#include <cassert>
+
+// 1 - work, 2 - stress testing
+#define MODE 2
+
+#define VERBOSE false
+
+
 
 template <typename T>
 std::ostream& operator << (std::ostream& os, const std::vector<T>& v) {
@@ -40,6 +48,36 @@ std::vector<size_t> get_v() {
     return v;
 }
 
+bool check(size_t k, std::vector<size_t>& sub) {
+    size_t k_local = 0;
+    for (const auto& e : sub) {
+        if (e == 1) { k_local++; }
+        if (k_local > k) {
+            return false;
+        }
+    }
+
+    return k_local == k;
+}
+
+size_t fast(size_t k, std::vector<size_t>& v) {
+    size_t sum = 0;
+    for (auto l = 0; l < v.size(); ++l) {
+        size_t r = l;
+        do {
+            std::vector<size_t> sub{v.begin() + l, v.begin() + r + 1};
+            if (VERBOSE) { std::cout << "sub = " << sub << ", l = " << l << ", r = " << r << std::endl; }
+            if (check(k, sub)) {
+                sum++;
+                if (VERBOSE) { std::cout << "true! sum = " << sum << std::endl; }
+            }
+            r++;
+        } while (r < v.size());
+    }
+
+    return sum;
+}
+
 size_t greedy(size_t k, std::vector<size_t>& v) {
     size_t sum = 0;
     for (auto l = 0; l < v.size(); ++l) {
@@ -55,14 +93,60 @@ size_t greedy(size_t k, std::vector<size_t>& v) {
     return sum;
 }
 
+
+void stress_testing() {
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    const size_t MIN_K = 0, MAX_K = 10;
+    std::uniform_int_distribution<> dist_K(MIN_K, MAX_K);
+
+    const size_t MIN_VAL = 0, MAX_VAL = 1;
+    std::uniform_int_distribution<> dist_VAL(MIN_VAL, MAX_VAL);
+
+    const int MIN_L = 1, MAX_L = 10000;
+    std::uniform_int_distribution<> dist_L(MIN_L, MAX_L);
+
+    while (true) {
+        std::cout << "======================" << std::endl;
+
+        size_t k = dist_K(gen);
+        size_t length = dist_L(gen);
+
+        std::vector<size_t> v;
+        for (auto i = 0; i < length; ++i) { v.push_back(dist_VAL(gen)); }
+
+        auto res_greedy = 1; //greedy(k, v);
+        auto res_fast = fast(k, v);
+
+        std::cout << "k = " << k << ", v = " << v <<
+                  ", res_greedy = " << res_greedy <<", res_fast = " << res_fast << std::endl;
+
+//        assert(res_greedy == res_fast);
+
+        v.clear();
+    }
+}
+
+
+
 int main() {
+
+#if MODE == 1
 
     auto k = get_k();
 //    std::cout << k << std::endl;
     auto v = get_v();
 //    std::cout << v << std::endl;
 
-    std::cout << greedy(k, v) << std::endl;
+    //    std::cout << greedy(k, v) << std::endl;
+    std::cout << fast(k, v) << std::endl;
+
+#elif MODE == 2
+    stress_testing();
+
+#endif
 
     return 0;
 }
