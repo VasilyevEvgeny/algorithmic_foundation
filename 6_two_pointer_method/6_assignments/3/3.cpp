d67e5e4fdfb1b4ccb9d5571b0bb8d32453aa496d
evasilyev <vasilev.evgeniy@physics.msu.ru>
2020-03-13 23:07:27 +0300

    6_two_pointer_method__6_assignments__3

--- 6_two_pointer_method/6_assignments/3/3.cpp
+++ 6_two_pointer_method/6_assignments/3/3.cpp
@@ -8,14 +8,14 @@
 #include <chrono>
 #include <cassert>
 #include <deque>
+#include <map>
 
 using tt = std::chrono::steady_clock;
 
-// 1 - work, 2 - stress testing
+// 1 - work, 2 - manual testing, 3 - stress testing
 #define MODE 1
 
-#define VERBOSE true
-
+#define VERBOSE false
 
 
 template <typename T>
@@ -32,27 +32,11 @@ std::ostream& operator << (std::ostream& os, const std::deque<T>& v) {
 }
 
 
-
-
-/*
-std::vector<size_t> get_v(std::string& s) {
-    std::vector<size_t> v;
-    for (auto e : s) { v.push_back(e - '0'); }
-
-    return v;
+bool is_in_units_coords(size_t pos, std::deque<size_t>& units_coords) {
+    auto size = units_coords.size();
+    if (!size) { return false; }
+    else { return pos == units_coords[size - 1]; }
 }
-
-std::vector<size_t> get_units(std::vector<size_t>& v) {
-    std::vector<size_t> units;
-    for (auto i = 0; i < v.size(); ++i) {
-        if (v[i] == 1) { units.push_back(i); }
-    }
-
-    return units;
-}
-*/
-
-
 
 size_t fast(size_t k, std::string& s) {
     size_t sum = 0;
@@ -75,22 +59,39 @@ size_t fast(size_t k, std::string& s) {
 
         std::deque<size_t> units_coords;
         size_t l = 0, r = 0, n_units = 0, first_unit = 0, last_unit = 0;
-        size_t n_it = 0;
+//        size_t n_it = 0;
         while (l < s.length() && r < s.length()) {
+            r = std::max(l, r);
+
+            // initialize coefficients
             while (n_units <= k && r < s.length()) {
-                if (s[r] == '1') {
-                    if (!n_units) { first_unit = r; }
-                    if (n_units == k - 1) { last_unit = r; }
-                    if (n_units <= k - 1) {
+                if (n_units < k) {
+                    if (s[r] == '1' && !is_in_units_coords(r, units_coords)) {
+                        if (!n_units) { first_unit = r; }
+                        if (n_units == k - 1) { last_unit = r; }
                         ++n_units;
                         units_coords.push_back(r);
+
+                        if (VERBOSE) { std::cout << "s[" << r << "] = 1, n_units = " << n_units << ", units_coords = " << units_coords << std::endl; }
+                    }
+                    if (r < s.length() - 1) { ++r; }
+                    else { break; }
+                    if (VERBOSE) { std::cout << "1 -> increase r to " << r << std::endl; }
+                }
+                else {
+                    if (VERBOSE) { std::cout << "n_units = k = " << n_units << "!" << std::endl; }
+                    if (s[r] == '0') {
+                        if (r < s.length() - 1) { ++r; }
+                        else { break; }
+                        if (VERBOSE) { std::cout << "2 -> increase r to " << r << std::endl; }
+                    }
+                    else {
+                        --r;
+                        if (VERBOSE) { std::cout << "s[" << r << "] = 1, make r = " << r << " and break!" << std::endl; }
+                        break;
                     }
                 }
-                if (r == s.length() - 1) { break; }
-                if (n_units == k && s[r + 1] == '1') { break; }
-                ++r;
             }
-
             if (n_units < k) { break; }
 
             size_t n_before = first_unit - l;
@@ -98,144 +99,25 @@ size_t fast(size_t k, std::string& s) {
 
             sum += (n_before + 1) * (n_after + 1);
 
-            if (VERBOSE) { std::cout << "l = " << l << ", r = " << r << ", first_unit = " << first_unit <<
+            if (VERBOSE) { std::cout << "===============> l = " << l << ", r = " << r << ", first_unit = " << first_unit <<
                                      ", last_unit = " << last_unit << ", n_before = " << n_before << ", n_after = " << n_after <<
                                      ", units_coords = " << units_coords << ", SUM = " << sum << std::endl; }
 
-
-
             l = first_unit + 1;
-            r = std::min(s.length() - 1, r + 1);
-            if (l == r && k != 1) { break; }
-
             --n_units;
             units_coords.pop_front();
             if (!units_coords.empty()) { first_unit = units_coords[0]; }
 
-            if (VERBOSE) { std::cout << "n_units = " << n_units << ", units_coords = " << units_coords <<
+            if (VERBOSE) { std::cout << "==> n_units = " << n_units << ", units_coords = " << units_coords <<
             ", l = " << l << ", first_unit = " << first_unit << std::endl; }
 
-            if (n_it == 10) { exit(-1); }
-            n_it++;
+//            if (n_it == 10) { exit(-1); }
+//            n_it++;
         }
     }
 
     return sum;
 }
-
-
-
-
-
-//size_t fast(size_t k, std::string& s) {
-//    size_t sum = 0;
-//    if (!k) {
-//        size_t n = 0;
-//        for (auto i = 0; i < s.length(); ++i) {
-//            if (s[i] == '0') { n++; }
-//            else {
-//                sum += (1 + n) * n / 2;
-//                if (VERBOSE) { std::cout << "n = " << n << ", sum is changed to " << sum << std::endl; }
-//                n = 0;
-//            }
-//            if (i == s.length() - 1) {
-//                sum += (1 + n) * n / 2;
-//            }
-//        }
-//    }
-//    else {
-//        for (size_t l = 0; l < s.length(); ++l) {
-//
-//            // set left unit pos
-//            if (s[l] == '0') { continue; }
-//            size_t r = l;
-//            if (VERBOSE) { std::cout << "l = r = " << l << std::endl; }
-//
-//            // find right unit pos
-//            size_t k_local = (s[r] == '1') ? 1 : 0;
-//            while (k_local < k && r < s.length() - 1) {
-//                r++;
-//                if (s[r] == '1') { k_local++; }
-//                if (VERBOSE) { std::cout << "k_local = " << k_local << ", l = " << l << ", r = " << r << std::endl; }
-//            }
-//            if (k_local < k) { break; }
-//
-//            // find n_before
-//            size_t n_before = 0, before = l;
-//            if (l) {
-//                while (s[before - 1] == '0' && before > 0) {
-//                    before--;
-//                }
-//            }
-//            n_before = l - before;
-//            if (VERBOSE) { std::cout << "n_before = " << n_before << std::endl; }
-//
-//            // find n_after
-//            size_t n_after = 0, after = r;
-//            if (r < s.length() - 1) {
-//                while (s[after + 1] == '0' && after < s.length() - 1) {
-//                    after++;
-//                }
-//            }
-//            n_after = after - r;
-//            if (VERBOSE) { std::cout << "n_after = " << n_after << std::endl; }
-//
-//            sum += (n_before + 1) * (n_after + 1);
-//            if (VERBOSE) { std::cout << "sum = " << sum << std::endl; }
-//        }
-//    }
-//
-//    return sum;
-//}
-
-
-//size_t fastest(size_t k, std::vector<size_t>& v) {
-//    auto units = get_units(v);
-//    if (VERBOSE) { std::cout << "units = " << units << std::endl; }
-//
-//    size_t sum = 0;
-//    if (k > units.size()) {
-//        return 0;
-//    }
-//    else if (!k) {
-//        size_t n = 0;
-//        for (auto i = 0; i < v.size(); ++i) {
-//            if (v[i] == 0) { n++; }
-//            else {
-//                sum += (1 + n) * n / 2;
-//                if (VERBOSE) { std::cout << "n = " << n << ", sum is changed to " << sum << std::endl; }
-//                n = 0;
-//            }
-//            if (i == v.size() - 1) {
-//                sum += (1 + n) * n / 2;
-//            }
-//        }
-//    }
-//    else {
-//        bool out = false;
-//        for (auto l = 0, r = 0; l < units.size(); ++l, r = l) {
-//            while (r - l < k - 1) {
-//                if (r < units.size() - 1) { r++; }
-//                else { out = true; break; }
-//            }
-//            if (out) { break; }
-//
-//            size_t n_before = 0, n_after = 0;
-//
-//            if (!l) { n_before = units[0]; }
-//            else { n_before = units[l] - units[l - 1] - 1; }
-//
-//            if (r == units.size() - 1) { n_after = v.size() - units[units.size()-1] - 1; }
-//            else { n_after = units[r + 1] - units[r] - 1; }
-//
-//            sum += (n_before + 1) * (n_after + 1);
-//
-//            if (VERBOSE) { std::cout << "l = " << l << ", r = " << r << ", n_before = " << n_before << ", n_after = " << n_after << ", sum = " << sum << std::endl;}
-//        }
-//    }
-//
-//    return sum;
-//}
 
 
 size_t greedy(size_t k, std::string& s) {
@@ -254,18 +136,63 @@ size_t greedy(size_t k, std::string& s) {
 }
 
 
+void manual_testing() {
+    std::vector<std::map<std::string, std::string>> test_data = {
+
+            // simple
+            {{"k", "1"}, {"s", "0"}, {"true", "0"}},
+            {{"k", "1"}, {"s", "1"}, {"true", "1"}},
+            {{"k", "1"}, {"s", "11"}, {"true", "2"}},
+            {{"k", "1"}, {"s", "111"}, {"true", "3"}},
+            {{"k", "2"}, {"s", "1"}, {"true", "0"}},
+            {{"k", "2"}, {"s", "11"}, {"true", "1"}},
+            {{"k", "2"}, {"s", "111"}, {"true", "2"}},
+            {{"k", "2"}, {"s", "011"}, {"true", "2"}},
+            {{"k", "2"}, {"s", "110"}, {"true", "2"}},
+
+            // k = 1
+            {{"k", "2"}, {"s", "0001000"}, {"true", "0"}},
+            {{"k", "5"}, {"s", "100110010"}, {"true", "0"}},
+            {{"k", "1"}, {"s", "0001"}, {"true", "4"}},
+            {{"k", "1"}, {"s", "1010"}, {"true", "6"}},
+            {{"k", "1"}, {"s", "0010"}, {"true", "6"}},
+            {{"k", "1"}, {"s", "01000"}, {"true", "8"}},
+            {{"k", "1"}, {"s", "011011"}, {"true", "7"}},
+            {{"k", "1"}, {"s", "000010100001"}, {"true", "25"}},
+
+            // medium
+            {{"k", "2"}, {"s", "10011010"}, {"true", "9"}},
+    };
+
+    for (const auto& line : test_data) {
+        size_t k = std::stoi(line.at("k"));
+        std::string s = line.at("s");
+
+        size_t res_true = std::stoi(line.at("true"));
+        auto pred_greedy = greedy(k, s);
+        auto pred_fast = greedy(k, s);
+
+        std::cerr << "k = " << k << ", s = " << s << ", true = " << res_true <<  ", pred_greedy = " << pred_greedy <<
+        ", pred_fast = " << pred_fast << std::endl;
+
+        assert(res_true == pred_greedy);
+        assert(res_true == pred_fast);
+    }
+}
+
+
 void stress_testing() {
 
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    const size_t MIN_K = 0, MAX_K = 10;
+    const size_t MIN_K = 0, MAX_K = 1000;
     std::uniform_int_distribution<> dist_K(MIN_K, MAX_K);
 
     const size_t MIN_VAL = 0, MAX_VAL = 1;
     std::uniform_int_distribution<> dist_VAL(MIN_VAL, MAX_VAL);
 
-    const int MIN_L = 1, MAX_L = 10;
+    const int MIN_L = 1, MAX_L = 1000;
     std::uniform_int_distribution<> dist_L(MIN_L, MAX_L);
 
     while (true) {
@@ -283,7 +210,7 @@ void stress_testing() {
         std::cout << "k = " << k << ", s = " << s << std::endl;
 
         tt::steady_clock::time_point begin_greedy = tt::now();
-        auto res_greedy = greedy(k, s);
+        auto res_greedy = 0; //greedy(k, s);
         tt::steady_clock::time_point end_greedy = tt::now();
         auto duration_greedy = std::chrono::duration_cast<std::chrono::nanoseconds> (end_greedy - begin_greedy).count();
 
@@ -317,10 +244,13 @@ int main() {
     std::getline(std::cin, s);
     if (VERBOSE) { std::cout << "s = " << s << std::endl; }
 
-    std::cout << greedy(k, s) << std::endl;
+//    std::cout << greedy(k, s) << std::endl;
     std::cout << fast(k, s) << std::endl;
 
 #elif MODE == 2
+    manual_testing();
+
+#elif MODE == 3
     stress_testing();
 
 #endif
