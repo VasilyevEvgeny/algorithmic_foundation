e76575afbaf5b7cfa7f3bfa9be4c446154f588da
evasilyev <vasilev.evgeniy@physics.msu.ru>
2020-04-18 00:00:47 +0300

    6_two_pointer_method__6_assignments__6

--- 6_two_pointer_method/6_assignments/6/6.cpp
+++ 6_two_pointer_method/6_assignments/6/6.cpp
@@ -9,12 +9,13 @@
 #include <map>
 #include <sstream>
 #include <cassert>
+#include <stdexcept>
 #include <random>
 #include <chrono>
 #include <algorithm>
 
 // 1 - work, 2 - manual testing, 3 - stress testing
-#define MODE 3
+#define MODE 1
 // 1 - EOF, 2 - \n
 #define INPUT_MODE 1
 // print debug information or not
@@ -111,80 +112,38 @@ public:
         if (n < 3) { return generate_answer(0); }
 
         size k = 0;
+
+        // unique
         std::set<size> tmp(sticks_.begin(), sticks_.end());
         std::vector<size> sticks_unique(tmp.begin(), tmp.end());
-        size n_unique = sticks_unique.size();
-        std::map<size, bool> sticks_map;
 
+        size last_a = 0, last_b = 0;
         for (size i_a = 0; i_a < n - 2; ++i_a) {
-            for (size i = 0; i < n_unique; ++i) { sticks_map[sticks_unique[i]] = false; }
-
+            if (i_a) { if (sticks_[i_a] == sticks_[i_a - 1]) { continue; }}
             for (size i_b = i_a + 1; i_b < n - 1; ++i_b) {
-                if (!sticks_map.at(sticks_[i_b])) { sticks_map.at(sticks_[i_b]) = true; }
-                else { continue; }
+                if ((sticks_[i_a] == last_a) && (sticks_[i_b] == last_b)) { continue; }
+                if ((sticks_[i_b] == sticks_[i_b - 1]) && (sticks_[i_b] != sticks_[i_a])) { continue; }
 
-                std::set<size> s{sticks_.begin() + i_b + 1, sticks_.end()};
-                std::vector<size> cs_unique(s.begin(), s.end());
+                last_a = sticks_[i_a];
+                last_b = sticks_[i_b];
 
-                auto a = sticks_[i_a];
-                auto b = sticks_[i_b];
-                size i_c_left = binary_search_left(a, b, cs_unique);
-                size i_c_right = binary_search_right(a, b, cs_unique);
+                if (VERBOSE) { std::cout << "::=> sticks_[" << i_a << "] = " << sticks_[i_a] << ", sticks_[" << i_b << "] = " << sticks_[i_b] << std::endl;  }
 
-                if (VERBOSE) { std::cout << "a = " << a << ", b = " << b; }
+                size a = sticks_[i_a];
+                size b = sticks_[i_b];
+
+                size i_c_left = binary_search_left(a, b, sticks_unique);
+                size i_c_right = binary_search_right(a, b, sticks_unique);
+
+                if (VERBOSE) { std::cout << "a = " << a << ", b = " << b << ", i_c_left = " << i_c_left << ", i_c_right = " << i_c_right; }
 
                 if (i_c_left != -1) {
-                    if (VERBOSE) { std::cout << ", cs_unique[i_c_left] = " << cs_unique[i_c_left] << ", cs_unique[i_c_right] = "
-                                             << cs_unique[i_c_right]; }
+                    if (VERBOSE) { std::cout << ", sticks_unique[i_c_left] = " << sticks_unique[i_c_left] << ", sticks_unique[i_c_right] = " << sticks_unique[i_c_right]; }
                     k += i_c_right - i_c_left + 1;
                     if (VERBOSE) { std::cout << ", k --> " << k; }
-                }
-                else { if (VERBOSE) { std::cout << ", i_c_left is out of bounds!"; }}
+                } else { if (VERBOSE) { std::cout << ", i_c_left is out of bounds!"; }}
 
                 if (VERBOSE) { std::cout << std::endl; }
-
-            }
-        }
-
-        return generate_answer(k);
-    }
-
-    std::string solve_slow() {
-        std::sort(sticks_.begin(), sticks_.end());
-        size n = sticks_.size();
-
-        if (VERBOSE) { std::cout << "sticks = " << sticks_ << std::endl; }
-
-        if (n < 3) { return generate_answer(0); }
-
-        size k = 0;
-        std::set<std::vector<size>> ab;
-        for (size i_a = 0; i_a < n - 2; ++i_a) {
-            for (size i_b = i_a + 1; i_b < n - 1; ++i_b) {
-                if (ab.find({sticks_[i_a], sticks_[i_b]}) != ab.end()) { continue; }
-                else {
-                    ab.insert({sticks_[i_a], sticks_[i_b]});
-
-                    std::set<size> s{sticks_.begin() + i_b + 1, sticks_.end()};
-                    std::vector<size> cs_unique(s.begin(), s.end());
-
-                    auto a = sticks_[i_a];
-                    auto b = sticks_[i_b];
-                    size i_c_left = binary_search_left(a, b, cs_unique);
-                    size i_c_right = binary_search_right(a, b, cs_unique);
-
-                    if (VERBOSE) { std::cout << "a = " << a << ", b = " << b; }
-
-                    if (i_c_left != -1) {
-                        if (VERBOSE) { std::cout << ", cs_unique[i_c_left] = " << cs_unique[i_c_left] << ", cs_unique[i_c_right] = "
-                                  << cs_unique[i_c_right]; }
-                        k += i_c_right - i_c_left + 1;
-                        if (VERBOSE) { std::cout << ", k --> " << k; }
-                    }
-                    else { if (VERBOSE) { std::cout << ", i_c_left is out of bounds!"; }}
-
-                    if (VERBOSE) { std::cout << std::endl; }
-                }
             }
         }
 
@@ -193,74 +152,65 @@ public:
 
 private:
 
-    static size binary_search_left(size a, size b, std::vector<size>& cs_unique) {
-        if (VERBOSE) { std::cout << ".....BINARY SEARCH <<LEFT>>: a = " << a << ", b = " << b << ", cs_unique = " << cs_unique << std::endl; }
+    static size binary_search_left(size a, size b, std::vector<size>& sticks_unique) {
+        if (VERBOSE) { std::cout << ".....BINARY SEARCH <<LEFT>>: a = " << a << ", b = " << b << ", sticks_unique = " << sticks_unique << std::endl; }
 
-        size n = cs_unique.size();
+        size n = sticks_unique.size();
         if (n == 1) {
-            bool is_triangle_obtuse = is_obtuse(a, b, cs_unique[0]);
+            bool is_triangle_obtuse = is_obtuse(a, b, sticks_unique[0]);
             if (is_triangle_obtuse) {
-                if (VERBOSE) { std::cout << ".....triangle with a = " << a << ", b = " << b << ", c = " << cs_unique[0] << " is obtuse!" << std::endl; }
-                return 0; }
+                if (VERBOSE) { std::cout << ".....triangle with a = " << a << ", b = " << b << ", c = " << sticks_unique[0] << " is obtuse!" << std::endl; }
+                return 0;
+            }
             else {
-                if (VERBOSE) { std::cout << ".....triangle with a = " << a << ", b = " << b << ", c = " << cs_unique[0] << " is not obtuse!" << std::endl; }
+                if (VERBOSE) { std::cout << ".....triangle with a = " << a << ", b = " << b << ", c = " << sticks_unique[0] << " is not obtuse!" << std::endl; }
                 return -1;
             }
         }
 
-        size it = 0;
-        auto max_it = 10;
-        size i_l = 0, i_r = cs_unique.size() - 1;
+        size i_l = 0, i_r = sticks_unique.size() - 1;
         while (i_l <= i_r) {
             if (VERBOSE) { std::cout << ".....i_l = " << i_l << ", i_r = " << i_r << std::endl; }
             auto i_m = i_l + (i_r - i_l) / 2;
-            auto m = cs_unique[i_m];
-            if (VERBOSE) { std::cout << ".....cs_unique[i_m] --> " << m << std::endl; }
+            auto m = sticks_unique[i_m];
+            if (VERBOSE) { std::cout << ".....sticks_unique[i_m] --> " << m << std::endl; }
             if (is_obtuse(a, b, m)) {
                 i_r = i_m - 1;
-                if (VERBOSE) { std::cout << ".....obtuse! cs_unique[i_l] = " << cs_unique[i_l] << ", cs_unique[i_r] --> " << cs_unique[i_r] << std::endl; }
+                if (VERBOSE) { std::cout << ".....obtuse! sticks_unique[i_l] = " << sticks_unique[i_l] << ", sticks_unique[i_r] --> " << sticks_unique[i_r] << std::endl; }
             }
             else {
                 i_l = i_m + 1;
-                if (VERBOSE) { std::cout << ".....not obtuse! cs_unique[i_r] = " << cs_unique[i_r] << ", cs_unique[i_l] --> " << cs_unique[i_l] << std::endl; }
+                if (VERBOSE) { std::cout << ".....not obtuse! sticks_unique[i_r] = " << sticks_unique[i_r] << ", sticks_unique[i_l] --> " << sticks_unique[i_l] << std::endl; }
             }
-
-            ++it;
-//            if (it > max_it) { exit(-1); }
         }
 
-        if (VERBOSE) { std::cout << ".....---> return cs_unique[i_r + 1] = " << cs_unique[i_r + 1] << std::endl; }
+        if (VERBOSE) { std::cout << ".....---> return sticks_unique[i_r + 1] = " << sticks_unique[i_r + 1] << std::endl; }
 
         auto res = i_r + 1;
         if (res > n - 1) { return -1; }
         else { return i_r + 1; }
     }
 
-    static size binary_search_right(size a, size b, std::vector<size>& cs_unique) {
-        if (VERBOSE) { std::cout << ".....BINARY SEARCH <<RIGHT>>: a = " << a << ", b = " << b << ", cs_unique = " << cs_unique << std::endl; }
+    static size binary_search_right(size a, size b, std::vector<size>& sticks_unique) {
+        if (VERBOSE) { std::cout << ".....BINARY SEARCH <<RIGHT>>: a = " << a << ", b = " << b << ", sticks_unique = " << sticks_unique << std::endl; }
 
-        size it = 0;
-        auto max_it = 10;
-        size i_l = 0, i_r = cs_unique.size() - 1;
+        size i_l = 0, i_r = sticks_unique.size() - 1;
         while (i_l <= i_r) {
             if (VERBOSE) { std::cout << ".....i_l = " << i_l << ", i_r = " << i_r << std::endl; }
             auto i_m = i_l + (i_r - i_l) / 2;
-            auto m = cs_unique[i_m];
-            if (VERBOSE) { std::cout << ".....cs_unique[i_m] --> " << m << std::endl; }
+            auto m = sticks_unique[i_m];
+            if (VERBOSE) { std::cout << ".....sticks_unique[i_m] --> " << m << std::endl; }
             if (exists(a, b, m)) {
                 i_l = i_m + 1;
-                if (VERBOSE) { std::cout << ".....exists! cs_unique[i_r] = " << cs_unique[i_r] << ", cs_unique[i_l] --> " << cs_unique[i_l] << std::endl; }
+                if (VERBOSE) { std::cout << ".....exists! sticks_unique[i_r] = " << sticks_unique[i_r] << ", sticks_unique[i_l] --> " << sticks_unique[i_l] << std::endl; }
             }
             else {
                 i_r = i_m - 1;
-                if (VERBOSE) { std::cout << ".....not exists! cs_unique[i_l] = " << cs_unique[i_l] << ", cs_unique[i_r] --> " << cs_unique[i_r] << std::endl; }
+                if (VERBOSE) { std::cout << ".....not exists! sticks_unique[i_l] = " << sticks_unique[i_l] << ", sticks_unique[i_r] --> " << sticks_unique[i_r] << std::endl; }
             }
-
-            ++it;
-//            if (it > max_it) { exit(-1); }
         }
 
-        if (VERBOSE) { std::cout << ".....---> return cs_unique[i_l - 1] = " << cs_unique[i_l - 1] << std::endl; }
+        if (VERBOSE) { std::cout << ".....---> return sticks_unique[i_l - 1] = " << sticks_unique[i_l - 1] << std::endl; }
 
         return i_l - 1;
     }
@@ -294,11 +244,14 @@ public:
                 {{"sticks", "3 4 5"}, {"true", "0"}},
                 {{"sticks", "3 4 6"}, {"true", "1"}},
                 {{"sticks", "3 4 5 6"}, {"true", "2"}},
+                {{"sticks", "4 4 4 7"}, {"true", "1"}},
+                {{"sticks", "5 6 6 9"}, {"true", "2"}},
                 {{"sticks", "4 5 8 9"}, {"true", "2"}},
                 {{"sticks", "3 4 5 5 6"}, {"true", "2"}},
 
                 // medium
                 {{"sticks", "5 9 11 12"}, {"true", "2"}},
+                {{"sticks", "8 4 9 4"}, {"true", "1"}},
 
         };
 
@@ -334,10 +287,10 @@ void stress_testing() {
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    const size MIN_N = 1, MAX_N = 7000;
+    const size MIN_N = 1, MAX_N = 100;
     std::uniform_int_distribution<> dist_N(MIN_N, MAX_N);
 
-    const size MIN_VAL = 1, MAX_VAL = 7000;
+    const size MIN_VAL = 1, MAX_VAL = 100;
     std::uniform_int_distribution<> dist_VAL(MIN_VAL, MAX_VAL);
 
     while (true) {
@@ -348,10 +301,10 @@ void stress_testing() {
         std::vector<size> sticks;
         for (size i = 0; i < n_sticks; ++i) { sticks.push_back(dist_VAL(gen)); }
 
-        std::cout << "sticks = " << sticks << std::endl;
+        std::cout << "n = " << sticks.size() << ", sticks = " << sticks << std::endl;
 
         tt::steady_clock::time_point begin_greedy = tt::now();
-        auto res_greedy = 0; //GreedySolver(sticks).solve();
+        auto res_greedy = GreedySolver(sticks).solve();
         tt::steady_clock::time_point end_greedy = tt::now();
         auto duration_greedy = std::chrono::duration_cast<std::chrono::nanoseconds> (end_greedy - begin_greedy).count();
 
@@ -363,7 +316,9 @@ void stress_testing() {
         std::cout << std::scientific << "T = " << duration_greedy * 1e-9 << " s :: res_greedy = " << res_greedy << std::endl;
         std::cout << std::scientific << "T = " << duration_fast * 1e-9 << " s :: res_fast = " << res_fast << std::endl;
 
-//        assert(res_greedy == res_fast);
+        if (duration_fast * 1e-9 > 3.0) { throw std::runtime_error("Fast solution is too slow: " + std::to_string(duration_fast * 1e-9) + " s"); }
+
+        assert(res_greedy == res_fast);
 
         sticks.clear();
     }
