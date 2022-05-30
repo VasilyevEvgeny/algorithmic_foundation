8cce86fc367cecdcf14cda34f93d0c68b5e8eaba
Evgeny Vasilyev <vasilev.evgeniy@physics.msu.ru>
2022-05-30 21:35:53 +0300

    10_06_06

--- 10_sorting/06_assignments/06/06.cpp
+++ 10_sorting/06_assignments/06/06.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <deque>
 #include <algorithm>
 
@@ -25,89 +24,113 @@ std::ostream& operator << (std::ostream& os, const std::deque<T>& v) {
 
 class Solution {
 public:
-    Solution(const size S, std::deque<size> users)
-            : S_(S)
-            , users_(std::move(users)) {
+    Solution(std::deque<std::deque<size>> commands)
+        : commands_(std::move(commands)) {
 
-        counting_sort(users_, MAX_DATA);
+        counting_sort();
 
-        if (VERBOSE) {
-            std::cout << users_ << std::endl;
-        }
-
-        size s = 0;
-        bool over = false;
-        for (size_t i = 0; i < users_.size(); ++i) {
-            s += users_[i];
-
-            if (VERBOSE) {
-                std::cout << s << std::endl;
-            }
-
-            if (s > S_) {
-                over = true;
-
-                if (VERBOSE) {
-                    std::cout << "over = " << over << std::endl;
-                }
-
-                if (i) {
-                    std::cout << i << std::endl;
-                }
-                else {
-                    std::cout << 0 << std::endl;
-                }
-                break;
-            }
-        }
-
-        if (!over) {
-            std::cout << users_.size() << std::endl;
-        }
 
     }
 
-    void counting_sort(std::deque<size>& arr, const size max_element) {
-        std::deque<size> cnt(max_element);
-        for (size_t i = 0; i < arr.size(); ++i) {
-            cnt[arr[i] - 1]++;
+    void counting_sort() {
+        std::deque<std::deque<std::deque<size>>> cnt(MAX_S, std::deque<std::deque<size>>());
+        for (const auto& command : commands_) {
+            if (VERBOSE) {
+                std::cout << "...command: " << command << std::endl;
+            }
+            cnt[command[0]].push_back(command);
         }
 
-        for (size_t i = 0, k = 0; i < cnt.size(); ++i) {
-            while (cnt[i]) {
-                arr[k++] = i + 1;
-                cnt[i]--;
+        if (VERBOSE) {
+            std::cout << "Cnt array:" << std::endl;
+            for (const auto& s_commands : cnt) {
+                std::cout << "[ ";
+                for (const auto& command : s_commands) {
+                    std::cout << command << " ";
+                }
+                std::cout << " ]" << std::endl;
             }
         }
+
+
+        for (auto& s_command : cnt) {
+            std::sort(s_command.begin(),
+                      s_command.end(),
+                      [](const std::deque<size>& lhs, const std::deque<size>& rhs){
+                if (lhs[1] == rhs[1]) {
+                    return lhs[2] < rhs[2];
+                } else {
+                    return lhs[1] < rhs[1];
+                }
+            });
+        }
+
+
+
+        if (VERBOSE) {
+            std::cout << "Cnt array sorted:" << std::endl;
+            for (const auto& s_commands : cnt) {
+                std::cout << "[ ";
+                for (const auto& command : s_commands) {
+                    std::cout << command << " ";
+                }
+                std::cout << " ]" << std::endl;
+            }
+        }
+
+        for (int64_t i = cnt.size() - 1, k = 0; i >= 0; --i) {
+            while (!cnt[i].empty()) {
+                commands_[k++] = cnt[i][0];
+                cnt[i].pop_front();
+            }
+        }
+
+        if (VERBOSE) {
+            std::cout << "Sorted data:" << std::endl;
+            for (const auto& e : commands_) {
+                std::cout << e << std::endl;
+            }
+        }
+
+        for (const auto& e : commands_) {
+            std::cout << e[2] + 1 << " ";
+        }
+        std::cout << std::endl;
     }
 
 private:
-    std::deque<size> users_;
-    const size S_;
+    std::deque<std::deque<size>> commands_;
 
-    const size MAX_DATA = 300;
+    const size MAX_S = 101;
 };
 
 
 int main() {
 
-    size S = 0, N = 0;
-    std::cin >> S >> N;
+    size_t N = 0;
+    std::cin >> N;
+
+    std::deque<std::deque<size>> commands(N);
+    for (size_t i = 0; i < N; ++i) {
+        std::deque<size> command(3);
+        for (size_t j = 0; j < 2; ++j) {
+            std::cin >> command[j];
+        }
+        command[2] = i;
+        if (VERBOSE) {
+            std::cout << "command: " << command << std::endl;
+        }
+        commands[i] = command;
+    }
 
     if (VERBOSE) {
-        std::cout << "S =" << S << ", N =" << N << std::endl;
+        std::cout << "Initial data:" << std::endl;
+        for (const auto& e : commands) {
+            std::cout << e << std::endl;
+        }
     }
 
-    std::deque<size> users(N);
-    for (size i = 0; i < N; ++i) {
-        std::cin >> users[i];
-    }
-
-    if (VERBOSE) {
-        std::cout << users << std::endl;
-    }
-
-    Solution solution(S, users);
+    Solution solution(commands);
 
     return 0;
 }
