2b131a6afa8bd158366270d9f3ee00ae3876907c
Evgeny Vasilyev <vasilev.evgeniy@physics.msu.ru>
2022-06-01 12:36:54 +0300

    10_06_08

--- 10_sorting/06_assignments/08/08.cpp
+++ 10_sorting/06_assignments/08/08.cpp
@@ -12,93 +12,106 @@ using size = uint64_t;
 
 template <typename T>
 std::ostream& operator << (std::ostream& os, const std::vector<T>& v) {
-  os << "[ ";
-  for (size_t i = 0; i < v.size(); ++i) {
-    os << v[i];
-    if (i != v.size() - 1) {
-      os << " ";
+    os << "[ ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i != v.size() - 1) {
+            os << " ";
+        }
     }
-  }
-  os << " ]";
-  return os;
+    os << " ]";
+    return os;
 }
 
 class Solution {
 public:
-  Solution(std::vector<size> arr)
-      : arr_(std::move(arr)) {
+    Solution(std::vector<size> arr)
+            : arr_(std::move(arr)) {
 
-    if (arr_.size() == 1) {
-      std::cout << 0 << std::endl;
+        if (arr_.size() == 1) {
+            std::cout << 0 << std::endl;
+        }
+
+        MergeSort(0, arr_.size() - 1);
+
+        std::cout << n_inv_ << std::endl;
     }
-
-    MergeSort(0, arr_.size() - 1);
-
-    std::cout << n_inv_ << std::endl;
-  }
 
 private:
 
-  void MergeSort(size left, size right) {
-    if (left < right) {
-      size middle = left + (right - left) / 2;
+    void MergeSort(size left, size right) {
+        if (left < right) {
+            size middle = left + (right - left) / 2;
 
-      MergeSort(left, middle);
-      MergeSort(middle + 1, right);
-      Merge(left, middle, right);
-    }
-  }
-
-  void Merge(size left, size middle, size right) {
-    std::vector<size> buf;
-    buf.reserve(right - left + 1);
-
-    for (size l = left, r = middle + 1; l <= middle || r <= right;) {
-      if (r > right || (arr_[l] < arr_[r] && l <= middle)) {
-        buf.push_back(arr_[l]);
-        ++l;
-
-
-      }
-      else {
-        n_inv_++;
-
-        buf.push_back(arr_[r]);
-        ++r;
-      }
+            MergeSort(left, middle);
+            MergeSort(middle + 1, right);
+            Merge(left, middle, right);
+        }
     }
 
-    /*for (size i = left; i <= right; ++i) {
-        arr_[i] = buf[i - left];
-    }*/
+    void Merge(size left, size middle, size right) {
+        std::vector<size> buf;
+        buf.reserve(right - left + 1);
 
-  }
+        if (VERBOSE) {
+            std::cout << "=======\nleft = " << left << ", middle = " << middle << ", right = " << right << std::endl;
+        }
+
+        for (size l = left, r = middle + 1; l <= middle || r <= right;) {
+            if (r > right || (arr_[l] < arr_[r] && l <= middle)) {
+                buf.push_back(arr_[l]);
+                ++l;
+                if (VERBOSE) {
+                    std::cout << "left. buf:" << buf << std::endl;
+                }
+            }
+            else {
+                if (arr_[l] > arr_[r] && l <= middle && r <= right) {
+                    n_inv_ += middle - l + 1;
+                    if (VERBOSE) {
+                        std::cout << "n_inv_ -> " << n_inv_ << std::endl;
+                    }
+                }
+
+                buf.push_back(arr_[r]);
+                ++r;
+                if (VERBOSE) {
+                    std::cout << "right. buf:" << buf << std::endl;
+                }
+            }
+        }
+
+        for (size i = left; i <= right; ++i) {
+            arr_[i] = buf[i - left];
+        }
+
+    }
 
 
-  std::vector<size> arr_;
-  size n_inv_ = 0;
+    std::vector<size> arr_;
+    size n_inv_ = 0;
 };
 
 
 int main() {
 
-  size_t N = 0;
-  std::cin >> N;
+    size_t N = 0;
+    std::cin >> N;
 
-  std::vector<size> arr(N);
-  for (size_t i = 0; i < N; ++i) {
-    std::cin >> arr[i];
-  }
-
-  if (VERBOSE) {
-    std::cout << "Initial data:" << std::endl;
-    for (const auto& e : arr) {
-      std::cout << e << " ";
+    std::vector<size> arr(N);
+    for (size_t i = 0; i < N; ++i) {
+        std::cin >> arr[i];
     }
-    std::cout << std::endl;
-  }
 
-  Solution solution(arr);
+    if (VERBOSE) {
+        std::cout << "Initial data:" << std::endl;
+        for (const auto& e : arr) {
+            std::cout << e << " ";
+        }
+        std::cout << std::endl;
+    }
 
-  return 0;
+    Solution solution(arr);
+
+    return 0;
 }
